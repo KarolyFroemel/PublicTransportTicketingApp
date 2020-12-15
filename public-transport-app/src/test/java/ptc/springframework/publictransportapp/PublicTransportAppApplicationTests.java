efe354bc100dd4379a806f4bc095eeaa091d130e
karoly.froemel <karoly.froemel@accenture.com>
2020-12-15 08:56:11 +0100

    structure changed

---
+++ public-transport-app/src/test/java/ptc/springframework/publictransportapp/PublicTransportAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ptc.springframework.publictransportapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicTransportAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
